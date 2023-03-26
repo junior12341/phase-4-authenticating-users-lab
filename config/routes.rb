94900893bedb5b242d63936a9839c2adf5d8a278
Hubert Jr <hubertjunior254@icloud.com>
2023-03-26 20:51:46 +0300

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   resources :articles, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  #Session routes
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
+  #User routes
+  get "/me", to: "users#show"
+
 end
